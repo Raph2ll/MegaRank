2c1cf192d5fe9d1cd6a34cb84502d475403624d2
Raph2ll <raphael.ph2ll@gmail.com>
2024-03-14 18:00:55 -0300

    feat: Models

---
+++ api/Models/User.cs
@@ -0,0 +1,16 @@
+using api.Models;
+
+namespace api.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string PasswordHash { get; set; }
+        public string Slug { get; set; }
+        public int RolesId { get; set; }
+        public Role Roles { get; set; }
+
+    }
+}
