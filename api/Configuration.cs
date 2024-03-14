67bda61ab27fb71d8a2882fbe05e35cedc6a877f
Raph2ll <raphael.ph2ll@gmail.com>
2024-03-14 19:57:46 -0300

    feat: program

---
+++ api/Configuration.cs
@@ -0,0 +1,5 @@
+public static class Configuration
+    {
+        //Token
+        public static string JwtKey { get; set; } = "12345678-1234-1234-1234-123456789123";
+    }
--- api/Program.cs
+++ api/Program.cs
@@ -1,6 +1,25 @@
-var builder = WebApplication.CreateBuilder(args);
-var app = builder.Build();
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
 
-app.MapGet("/", () => "Hello World!");
+namespace YourNamespace
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            CreateHostBuilder(args).Build().Run();
+        }
 
-app.Run();
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.ConfigureAppConfiguration((hostingContext, config) =>
+                    {
+                        config.AddJsonFile("secrets.json", optional: true, reloadOnChange: true);
+                    });
+                    webBuilder.UseStartup<Startup>();
+                });
+    }
+}
