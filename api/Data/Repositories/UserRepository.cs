607863779889f53f1cb02fa65580f0aa6c42a4a3
Raph2ll <raphael.ph2ll@gmail.com>
2024-03-16 13:23:46 -0300

    refactor: UserRepository

---
+++ api/Data/Repositories/UserRepository.cs
@@ -0,0 +1,127 @@
+using System;
+using System.Collections.Generic;
+using System.Data;
+using api.Models;
+using MySql.Data.MySqlClient;
+
+
+namespace api.Data.Repositories
+{
+    public class UserRepository
+    {
+        private readonly DataContext _connection;
+
+        public UserRepository(DataContext connection)
+        {
+            _connection = connection;
+        }
+
+        public void AddUser(User user)
+        {
+            using (var dbConnection = _connection.GetConnection())
+            {
+                dbConnection.Open();
+                using (var cmd = new MySqlCommand("INSERT INTO User (Name, Email, PasswordHash, Slug) VALUES (@Name, @Email, @PasswordHash, @Slug)",
+                    dbConnection))
+                {
+                    cmd.Parameters.AddWithValue("@Name", user.Name);
+                    cmd.Parameters.AddWithValue("@Email", user.Email);
+                    cmd.Parameters.AddWithValue("@PasswordHash", user.PasswordHash);
+                    cmd.Parameters.AddWithValue("@Slug", user.Slug);
+
+                    cmd.ExecuteNonQuery();
+                }
+            }
+        }
+        public List<User> GetAllUser()
+        {
+            var users = new List<User>();
+
+            using (var dbConnection = _connection.GetConnection())
+            {
+                dbConnection.Open();
+                using (var command = new MySqlCommand($"SELECT Name, Email, PasswordHash, Slug FROM MegaRank.User",
+                    dbConnection))
+                {
+                    using (var reader = command.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            var user = new User
+                            {
+                                Id = Convert.ToInt32(reader["Id"]),
+                                Name = reader["Name"].ToString(),
+                                Email = reader["Email"].ToString(),
+                            };
+
+                            users.Add(user);
+                        }
+                    }
+                }
+            }
+
+            return users;
+
+        }
+        public User GetUserById(int id)
+        {
+            using (var dbConnection = _connection.GetConnection())
+            {
+                dbConnection.Open();
+                using (var command = new MySqlCommand("SELECT * FROM MegaRank.User WHERE Id = @Id",
+                    dbConnection))
+                {
+                    command.Parameters.AddWithValue("@Id", id);
+
+
+                    using (var reader = command.ExecuteReader())
+                    {
+                        if (reader.Read())
+                        {
+                            return new User
+                            {
+                                Id = Convert.ToInt32(reader["Id"]),
+                                Name = Convert.ToString(reader["Name"]),
+                                Email = Convert.ToString(reader["Email"]),
+                                PasswordHash = Convert.ToString(reader["PasswordHash"]),
+                                Slug = Convert.ToString(reader["Slug"])
+                            };
+                        }
+                    }
+                }
+            }
+
+            return null;
+
+        }
+        public void Update(User updatedUser)
+        {
+            using (var dbConnection = _connection.GetConnection())
+            {
+                dbConnection.Open();
+                using (var command = new MySqlCommand("UPDATE MegaRank.User SET Name = @Name, Email = @Email WHERE Id = @Id",
+                    dbConnection))
+                {
+                    command.Parameters.AddWithValue("@Id", updatedUser.Id);
+                    command.Parameters.AddWithValue("@Name", updatedUser.Name);
+                    command.Parameters.AddWithValue("@Email", updatedUser.Email);
+
+                    command.ExecuteNonQuery();
+                }
+            }
+        }
+        public void Delete(int userId)
+        {
+            using (var dbConnection = _connection.GetConnection())
+            {
+                dbConnection.Open();
+                using (var command = new MySqlCommand("DELETE FROM MegaRank.User WHERE Id = @userId",
+                    dbConnection))
+                {
+                    command.Parameters.AddWithValue("@ProductId", userId);
+                    command.ExecuteNonQuery();
+                }
+            }
+        }
+    }
+}
