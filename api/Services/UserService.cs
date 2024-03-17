6255cfa150616c5c55a75fc6bec744ce02941559
Raph2ll <raphael.ph2ll@gmail.com>
2024-03-17 08:33:17 -0300

    feat: UserService

---
+++ api/Services/UserService.cs
@@ -0,0 +1,41 @@
+using api.Data.Repositories;
+using api.Models;
+using System.Collections.Generic;
+
+namespace api.Services
+{
+    public class UserService : IUserService
+    {
+        private readonly IUserRepository _userRepository;
+
+        public UserService(IUserRepository userRepository)
+        {
+            _userRepository = userRepository;
+        }
+
+        public void AddUser(User user)
+        {
+            _userRepository.AddUser(user);
+        }
+
+        public List<User> GetAllUsers()
+        {
+            return _userRepository.GetAllUser();
+        }
+
+        public User GetUserById(int id)
+        {
+            return _userRepository.GetUserById(id);
+        }
+
+        public void UpdateUser(User updatedUser)
+        {
+            _userRepository.Update(updatedUser);
+        }
+
+        public void DeleteUser(int userId)
+        {
+            _userRepository.Delete(userId);
+        }
+    }
+}
